d6431bd022644aa89983031c4a84f9d92a43d592
vpriscan <vedran.priscan.hr@gmail.com>
2019-03-17 18:17:32 +0100

    Add Kost source

---
+++ src/main/kotlin/hr/vpriscan/kost/Kost.kt
@@ -0,0 +1,64 @@
+@file:Suppress("SpellCheckingInspection")
+
+package hr.vpriscan.kost
+
+@DslMarker
+annotation class KsonMarker
+
+object Kost {
+
+    operator fun get(builder: KostArray.() -> Unit): MutableList<Any?> {
+        return KostArray()
+            .apply(builder)
+    }
+
+    operator fun invoke(builder: KostObject.() -> Unit): MutableMap<String, Any?> {
+        return KostObject()
+            .apply(builder)
+    }
+}
+
+@KsonMarker
+class KostArray : ArrayList<Any?>() {
+
+    object ElementHook
+
+    @Suppress("PropertyName")
+    val _a get() = ElementHook
+
+    operator fun ElementHook.minus(value: Any?) {
+        this@KostArray.add(value)
+    }
+
+    operator fun ElementHook.invoke(builder: KostObject.() -> Unit) {
+        KostObject()
+            .apply(builder)
+            .let { this@KostArray.add(it) }
+    }
+
+    operator fun ElementHook.get(builder: KostArray.() -> Unit) {
+        KostArray()
+            .apply(builder)
+            .let { this@KostArray.add(it) }
+    }
+}
+
+@KsonMarker
+class KostObject : LinkedHashMap<String, Any?>() {
+
+    operator fun String.minus(value: Any?) {
+        this@KostObject[this@minus] = value
+    }
+
+    operator fun String.invoke(builder: KostObject.() -> Unit) {
+        KostObject()
+            .apply(builder)
+            .let { this@KostObject[this@invoke] = it }
+    }
+
+    operator fun String.get(builder: KostArray.() -> Unit) {
+        KostArray()
+            .apply(builder)
+            .let { this@KostObject[this@get] = it }
+    }
+}
